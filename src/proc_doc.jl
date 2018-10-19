5ba8e6733b26a919605b04a33ffe32480ebd9fea
Matthew Gordon <mgordon@mygn.myriad.com>
2018-10-19 02:18:32 -0600

    Update file processing.

--- src/proc_doc.jl
+++ src/proc_doc.jl
@@ -1,32 +1,17 @@
-using TextAnalysis
-using DataStructures
-
-struct Node
-    name::String
-    binary::String
-    up::Int
-    down::Vector{Int}
-end
-
-function Node(name::String)
-    Node(name, "", 0, [0,0])
-end
-
+# if a file name is provided as String
 function makedicts(path::AbstractString, mincount::Int)
-    ngd = ngrams(FileDocument(path))
-    ret = _makedicts(ngd, mincount)
-    ret
+    return _makedicts(ngrams(FileDocument(path)), mincount)
 end
 
+# if a TextAnalysis type document is provided
 function makedicts(doc::AbstractDocument, mincount::Int)
-    ngd = ngrams(doc)
-    ret = _makedicts(ngd, mincount)
-    ret
+    return _makedicts(ngrams(doc), mincount)
 end
 
 function _makedicts(ngd::Dict, mincount::Int)
-    lexicn = _dropmin(ngd, mincount)
-    pq = PriorityQueue(lexicn)
+    words_counts = _dropmin(ngd, mincount)
+    pq = PriorityQueue(words_counts)
+    lexicn = collect(keys(words_counts))
     w2id = Dict{String, Int}()
     id2w = Dict{Int, String}()
     for (i,j) in enumerate(pq)
@@ -34,17 +19,17 @@ function _makedicts(ngd::Dict, mincount::Int)
         w2id[k] = i
         id2w[i] = k
     end
-    (pq, lexicn, w2id, id2w)
+    pq, lexicn, w2id, id2w
 end
 
 function _dropmin(ngd, mincount)
-    lexicn = Dict{String, Int}()
+    words_counts = Dict{String, Int}()
     for (i, j) in ngd
         if j > mincount
-            lexicn[i] = j
+            words_counts[i] = j
         else
             continue
         end
     end
-    return lexicn
+    return words_counts
 end
--- tests/proc_doc_tests.jl
+++ tests/proc_doc_tests.jl
@@ -1,36 +1,16 @@
-# using TextAnalysis
-#
-# path = "..\\data\\shakespear.txt"
-#
-# flags = [strip_corrupt_utf8,
-#         strip_case,
-#         strip_whitespace,
-#         strip_punctuation,
-#         strip_numbers,
-#         strip_html_tags]
-#
-# function gendoc(path)
-#     sd = StringDocument(text(FileDocument(path)))
-#     for flag in flags
-#         prepare!(sd, flag)
-#     end
-# end
-using Test
+using DataStructures
 
-include("..\\src\\proc_doc.jl")
-include(".\\utils\\proc_doc_utils.jl")
-
-datapath = ".\\data\\shakespear.txt"
-doc = gen_test_doc(datapath)
-
-test_output = makedicts(doc, 10)
+proc_output = makedicts(doc, 10)
 
 @testset "document processing" begin
 
-    @test typeof(test_output[1]) == PriorityQueue{String,Int64,Base.Order.ForwardOrdering}
-    @test length(test_output[1]) == 277
-    @test typeof(test_output[2]) == Dict{String, Int64}
-    @test typeof(test_output[3]) == Dict{String, Int64}
-    @test typeof(test_output[4]) == Dict{String, Int64}
+    @test typeof(proc_output[1]) == PriorityQueue{String,Int64,Base.Order.ForwardOrdering}
+    @test length(proc_output[1]) == 277
+    @test typeof(proc_output[2]) == Array{String, 1}
+    @test length(proc_output[2]) == 277
+    @test typeof(proc_output[3]) == Dict{String, Int}
+    @test length(proc_output[3]) == 277
+    @test typeof(proc_output[4]) == Dict{Int, String}
+    @test length(proc_output[4]) == 277
 
 end
