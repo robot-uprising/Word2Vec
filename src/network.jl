f1866595a4fc33e91b27da1fd3d93b63275d21b6
Mat Gordon <msgordon@umich.edu>
2018-12-05 17:07:17 -0700

    WIP update of W2VNetwork interface

--- src/network.jl
+++ src/network.jl
@@ -1,16 +1,12 @@
-
-
 mutable struct W2VNetwork{S<:AbstractString, T<:Real, H<:Integer}
     wv::WordVectors{S,T,H} #WordVectors struct
-    ht::Union{HuffTree{H}, Nothing} #Huffman Tree struct
+    ht::Union{HuffmanTree{H}, Nothing} #Huffman Tree struct
     ov::Union{AbstractArray{T,2}, Nothing} #Output vectors
+    #frequency table???
 end
 
 # fix this so we just word with array vocab
-function W2VNetwork(doc, mincount::Integer, dims::Integer)
-    @debug "Commencing network build"
-    ht, vocab_hash = _createbinarytree(doc, mincount)
-    @debug "generating inital vector states"
+function W2VNetwork(ht::HuffmanTree, vocab_hash::Dict, dims::Integer)
     vocab_size = length(vocab_hash)
     vocab = Array{String}(undef, vocab_size)
     for (i, j) in vocab_hash
@@ -20,7 +16,23 @@ function W2VNetwork(doc, mincount::Integer, dims::Integer)
     return W2VNetwork(wv, ht, randn(vocab_size-1, dims), vocab_size)
 end
 
+function dump_model!(wn::W2VNetwork)
+    wn.ht = nothing
+    wn.ov = nothing
+end
+
+function extract_vectors(wn::W2VNetwork)
+    return wn.wv
+end
+
+function extract_vectors!(wn:W2W2VNetwork)
+    wv = copy(wn.wv)
+    wn = nothing
+    return wv
+end
+
 # need to implement
+# maybe the word2vec stuff in a separate file
 #     word2vec - initializes and trains as directed
 #         train! - in place update of wieghts
 #             needs to be different based on :skipgram or :cbow
