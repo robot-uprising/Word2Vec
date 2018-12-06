7605d4367cad6ea163cdb5d4093b387778bc2df1
Mat Gordon <msgordon@umich.edu>
2018-12-05 17:04:00 -0700

    beginning of test cleanup

--- tests/huffman_tests.jl
+++ tests/huffman_tests.jl
@@ -1,19 +1,19 @@
 @testset "huffman tree" begin
 
-    ht, vocab_hash = _createbinarytree(docpath, 10)
-    @test typeof(ht) == HuffTree{Int}
+    ht, vocab_hash = HuffmanTree(docpath, 10)
+    @test typeof(ht) == HuffmanTree{Int}
     @test typeof(vocab_hash) == Dict{String, Int}
     @test length(ht.nodeparent) == 2*length(vocab_hash)-1
 
-    ht, vocab_hash = _createbinarytree(doc, 10)
+    ht, vocab_hash = HuffmanTree(doc, 10)
     @test length(ht.nodeparent) == 2*length(vocab_hash)-1
 
-    ht, vocab_hash = _createbinarytree(crps, 10)
+    ht, vocab_hash = HuffmanTree(crps, 10)
     @test length(ht.nodeparent) == 2*length(vocab_hash)-1
 
     ary = Array{Array{Int64,1},1}()
     for i in keys(vocab_hash)
-        nodepath, branchpath = _rootpath(ht, vocab_hash, i)
+        nodepath, branchpath = rootpath(ht, vocab_hash, i)
         push!(ary, branchpath)
     end
     @test allunique(ary)
@@ -22,10 +22,10 @@
     prior = [128,64,32,16,8,4,2,1]
 
     pq, vocab_hash = _dicts(Dict(zip(key, prior)), 0)
-    ht = HuffTree(_binarytree(pq, vocab_hash)...)
+    ht = HuffmanTree(_binarytree(pq, vocab_hash)...)
     ary1 = Array{Tuple{Array{Int64,1},Array{Int64,1}},1}()
     for i in key
-        push!(ary1, _rootpath(ht, vocab_hash, i))
+        push!(ary1, rootpath(ht, vocab_hash, i))
     end
     paths = [([15], [0]),
             ([14, 15], [0, 1]),
--- tests/utils.jl
+++ tests/utils.jl
@@ -1,11 +1,6 @@
 using TextAnalysis
 
-flags = [strip_corrupt_utf8,
-        strip_case,
-        strip_whitespace,
-        strip_punctuation,
-        strip_numbers,
-        strip_html_tags]
+
 
 function gen_test_doc(path)
     sd = StringDocument(text(FileDocument(path)))
