0f41f110a847f21731511bc8f65914e3023bd2ce
Mat Gordon <msgordon@umich.edu>
2019-03-21 21:19:25 -0600

    Updated tests for inputprocessing

--- tests/inputprocessing_tests.jl
+++ tests/inputprocessing_tests.jl
@@ -2,12 +2,20 @@ using DataStructures
 
 @testset "input processing" begin
 
-    sentences = [["hi", "tests"], ["tests", "are", "fun"]]
-    true_unigrams = Dict("tests" => 2, "hi" => 1, "are" => 1, "fun" => 1)
+    sentences = [["hi", "tests", "world"], ["tests", "are", "sure", "fun"]]
+    true_unigrams = Dict("tests" => 2, "hi" => 1, "world" => 1, "are" => 1, "sure" => 1, "fun" => 1)
+    true_contexts = [("hi", "tests", "world"), ("tests", "are", "sure"), ("are", "sure", "fun")]
 
-    pq, vocab_hash, contexts, unigrams = _process_input(sentences, 1, 2)
+    pq, vocab_hash, contexts, unigrams = _process_input(sentences, 1, 1)
 
-    @test typeof(pq) == PriorityQueue
+    @test typeof(pq) == PriorityQueue{String,Int64,Base.Order.ForwardOrdering}
     @test unigrams == true_unigrams
+    test_contexts = []
+    for line in contexts
+        for context in line
+            push!(test_contexts, context)
+        end
+    end
+    @test test_contexts == true_contexts
 
 end
--- tests/runtests.jl
+++ tests/runtests.jl
@@ -1,5 +1,6 @@
 using Test
 using Word2Vec
+using Word2Vec: _process_input
 
 include("inputprocessing_tests.jl")
 include("huffman_tests.jl")
