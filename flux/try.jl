a6b5a3554a915b2342d25706356e31c03e9d3a3c
Mat Gordon <msgordon@umich.edu>
2019-03-29 13:56:27 -0600

    update flux example - to be deleted when train.jl is finalized

--- flux/try.jl
+++ flux/try.jl
@@ -9,11 +9,11 @@ ov = param(randn(10000,200)/3)
 # define functions
 
 # nodeprob: returns probability you are going the right way at a node
+# ov = the output vector matrix a Flux param
 # h = internal representation
 # node = the current node number
 # branch is the direction to branch from the node
-# also requires a param(ov) (output vector) matrix
-function nodeprob(h, node, branch)
+function nodeprob(ov, h, node, branch)
     v = ov[node,:]
     branch == 1 ? ◫ = 1 : ◫ = -1
     return σ(◫*dot(v,h))
@@ -23,21 +23,23 @@ end
 # h = internal representation
 # nodes = array of nodes in path to output word from Huffman tree
 # branches = array of directions (0,1) to branch from each node in nodes
-function prob(h, nodes, branches)
-    reduce(*, map((node, branch)->nodeprob(h, node, branch), nodes, branches))
+function prob(ov, h, nodes, branches)
+    e = reduce(*, map((node, branch)->(nodeprob(ov, h, node, branch)), nodes, branches))
 end
 
 # loss is the -log of the probability(output word|input word)
-loss(h, nodes, branches) = -log(prob(h, nodes, branches))
+loss1(ov, h, nodes, branches) = -1*log(prob(ov, h, nodes, branches))
+loss2(ov, h, nodes, branches) = -1*reduce(+, map((node, branch)->log((nodeprob(ov, h, node, branch))), nodes, branches))
 
 
 # try it out
-h = randn(200)/3
+h = param(randn(200)/3)
 nodes = [200,300,400,500]
 branches = [0,0,1,0]
 
-for i in 1:3
-    grad1 = Tracker.gradient(()->loss(h, nodes, branches), Params([ov]))
+for i in 1:30
+    println(loss1(ov, h, nodes, branches))
+    grad1 = Tracker.gradient(()->loss1(ov, h, nodes, branches), Params([ov]))
     a = grad1[ov]
     update!(ov, -0.1*a)
 end
