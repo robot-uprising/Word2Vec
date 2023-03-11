d6b6cb34962df0681bf1a0da786d8189a63d24ec
Mat Gordon <msgordon@umich.edu>
2023-03-11 16:04:38 -0700

    Fix document loading error for tokenize

--- src/doctools/tokenize.jl
+++ src/doctools/tokenize.jl
@@ -7,18 +7,18 @@ using WordTokenizers
 as a String and returns an Array of tokenized sentences.
 """
 function tokenize_document(path::T) where T<: AbstractString
-    tokenized = open(path) do file
-        text = replace(read(file, String), r"\n+|\r+|\s+" => " ")
-        sentences = tokenize.(split_sentences(text))
-        tokenized = Array{Array{AbstractString, 1}, 1}()
-        for sentence in sentences
-            no_punct = Array{AbstractString, 1}()
-            for word in sentence
-                occursin(r"[!\"#$%&\'()*+,-./:;<=>?@\[\\\]^_`\{\|\}~]+", word) ?
+    file = open(path, "r")
+    text = replace(read(file, String), r"\n+|\r+|\s+" => " ")
+    sentences = tokenize.(split_sentences(text))
+    tokenized = Array{Array{AbstractString, 1}, 1}()
+    for sentence in sentences
+        no_punct = String[]
+        for word in sentence
+            occursin(r"[!\"#$%&\'()*+,-./:;<=>?@\[\\\]^_`\{\|\}~]+", word) ?
                                                 continue : push!(no_punct, word)
-            end
-            push!(tokenized, lowercase.(no_punct))
         end
+        push!(tokenized, lowercase.(no_punct))
     end
+    close(file)
     return tokenized
 end
