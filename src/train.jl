04bb306cfa8216d033a83bd29c879340d42bbf67
Mat Gordon <msgordon@umich.edu>
2023-03-17 02:43:45 -0600

    Working CBOW training!

--- src/train.jl
+++ src/train.jl
@@ -1,17 +1,44 @@
 using StatsFuns: logistic
 σ = logistic
 
-function _train!(model, ::Val{:cbow})
-    @show model
-    @show data
-    println("cbow not yet implemented")
+function _train!(model, η, ::Val{:cbow})
+    for context in model.data.ordering #loop over the dataset
+        (y,x) = iterate(model.data.xs[context[1]],context[2])[1]
+        h = _cbow_h(model, x)
+
+        #calculate the error to update wv
+        wv_err = _wv_error_per_output(model, h, y) 
+
+        #loop over the hs weights to update
+        _update_all_nodes!(model, h, y, η)
+
+        #update the input vectors
+        _cbow_update_wv!(model, x, η, wv_err)
+    end
 end
 
 function _train!(model, ::Val{:skipgram})
+    (x,y) = iterate(model.data.xs[context[1]],context[2])[1]
     @show model
     @show data
     println("skipgram not yet implemented")
 end
+
+#cbow model input vector creation
+function _cbow_h(model, input)
+    h = zeros(size(model.wv.vectors)[1])
+    for word in input
+        h = h + model.wv.vectors[:,word]
+    end
+    return h/length(input)
+end
+
+function _cbow_update_wv!(model, x, η, wv_err)
+    for word in x
+        model.wv.vectors[:,word] = model.wv.vectors[:,word] - 1/length(x)*η*wv_err
+    end
+end
+
 
 # basic math for updating hidden -> out heirarchical softmax nodes
 # h is the wv (:skipgram), or average wv (:cbow)
@@ -25,7 +52,14 @@ end
 # uses the function _hs_update_val to update the weight vector for a hs node in place
 # eq. 51 from the Rong paper
 function _update_hs_node!(model, h, node, t, η)
-    model.hs.weights[:,node] = model.hs.weights[:,node] - _hs_update_val(model.wv.vectors[:,h], model.hs.weights[:,node], t, η) 
+    model.hs.weights[:,node] = model.hs.weights[:,node] - _hs_update_val(h, model.hs.weights[:,node], t, η) 
+end
+
+# function to loop over an entire path through the hs tree for a y value
+function _update_all_nodes!(model, h, y, η)
+    for (node, branch) in zip(model.hs.nodepaths[y], model.hs.branchpaths[y])
+        _update_hs_node!(model, h, node, branch, η)
+    end
 end
 
 # basic math for calculating the per node error which will be summed (in another function)
@@ -44,7 +78,7 @@ end
 function _wv_error_per_output(model, h, y)
     err = zeros(size(model.wv.vectors)[1]) #make a vector of wv size to accumulate error
     for (node, branch) in zip(model.hs.nodepaths[y], model.hs.branchpaths[y])
-        err = err + _wv_error_per_node(h, node, branch)
+        err = err + _wv_error_per_node(h, model.hs.weights[:,node], branch)
     end
     return err
 end
